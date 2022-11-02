0f33fbf0ebe24195e550250f7936b67dd2f16513
HBalu1026 <71315012+HBalu1026@users.noreply.github.com>
2022-11-02 23:00:33 +0100

    Add files via upload

---
+++ javascript/js.js
@@ -0,0 +1,34 @@
+function elkuld() {
+    var nev_bekuld=document.getElementById("nev_bekuld").value;
+    document.getElementById("nev").innerHTML=nev_bekuld;
+
+    var cim_bekuld=document.getElementById("cim_bekuld").value;
+    document.getElementById("cim").innerHTML=cim_bekuld;
+
+    var tel_bekuld=document.getElementById("tel_bekuld").value;
+    document.getElementById("tel").innerHTML=tel_bekuld;
+}
+
+function beszinez_nev() {
+    document.getElementById("").style.backgroundColor= "grey";
+}
+
+function beszinez_cim() {
+    document.getElementById("").style.backgroundColor= "grey";
+}
+
+function beszinez_tel() {
+    document.getElementById("").style.backgroundColor= "grey";
+}
+
+function kiszinez_nev() {
+    document.getElementById("").style.backgroundColor= "white";
+}
+
+function kiszinez_cim() {
+    document.getElementById("").style.backgroundColor= "white";
+}
+
+function kiszinez_tel() {
+    document.getElementById("").style.backgroundColor= "white";
+}
